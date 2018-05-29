af30e8fd2cf2bde344ddb66d6eae66001bc33bd9
cami <evacamilla.s@gmail.com>
2018-05-29 16:27:38 +0200

    Add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header(){
+    return(
+        <header className="App-header">
+            <h1 className="App-title">Clicker Game</h1>
+        </header>
+    );
+}
+
+export default Header;
