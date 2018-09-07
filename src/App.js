c2aef9aa94f5436fab5bfc076f9863dc885a8d24
cami <evacamilla.s@gmail.com>
2018-09-07 16:22:25 +0200

    Function for updating points per second

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header.js';
+import PlayerStats from './components/PlayerStats.js';
 import NameForm from './components/NameForm.js';
 import Button from './components/Button.js';
 import CellPhone from './components/CellPhone.js';
@@ -17,28 +18,39 @@ class App extends Component {
     totalPoints: 0,
     fame: 0,
     pointsPerClick: 1,
-    pointsPerSecond: 0
+    pointsPerSecond: 0,
+    dateStart: "",
+    automaticPointsMessage: ""
   }
 
   handleNameForm = (value) => {
     this.setState({playerIsSet: true, nameOfPlayer: value});
   }
 
-  increaseTotalPoints = (props) => {
-    this.setState({totalPoints: this.state.totalPoints + props});
+  increaseTotalPoints = () => {
+    this.setState({totalPoints: this.state.totalPoints +  this.state.pointsPerClick});
   }
 
-  //this + increaseTotalPoints should be made as one function
   removeFromTotalPoints = (cost) => {
     this.setState({totalPoints: this.state.totalPoints - cost});
   }
 
-  //make one function out of these??
   updatePointsPerClick = (effect) => {
     this.setState({pointsPerClick: this.state.pointsPerClick + effect});
   }
 
   updatePointsPerSecond = (effect) => {
+    if (this.state.pointsPerSecond === 0) {
+      //if its the first time user buys automatically generated points, 
+      //store the time of the event in this.state to be able to calculate seconds passed and increase total points based on that
+      this.setState({
+        dateStart: new Date(),
+        automaticPointsMessage: "Congratulations you bought your first automatic update!!"
+      });
+
+      console.log('first update: ' + this.state.dateStart);
+    }
+    
     this.setState({pointsPerSecond: this.state.pointsPerSecond + effect});
   }
 
@@ -48,35 +60,75 @@ class App extends Component {
     auto ? this.updatePointsPerSecond(effect) : this.updatePointsPerClick(effect);
   }
 
+  //WIP
+  //make every 10 seconds?
+  automaticUpdate = () => {
+    let dateNow = new Date();
+    let secondsPassed = dateNow - this.state.dateStart;
+    secondsPassed /= 1000;
+    let automaticallyEarnedPoints = secondsPassed * this.state.pointsPerSecond;
+    let newFame = automaticallyEarnedPoints + this.state.totalPoints;
+
+    this.setState({fame: newFame});
+    console.log("seconds passed " + secondsPassed);
+    console.log('automatically earned points ' + automaticallyEarnedPoints);
+  }
+
+
   render() {
+    const { nameOfPlayer, totalPoints, fame, pointsPerClick, pointsPerSecond, automaticPointsMessage } = this.state;
+
+    if(this.state.pointsPerSecond > 0) { 
+      setTimeout(this.automaticUpdate, 2500);
+    }
+
     return (
       <div className="App">
-        {this.state.playerIsSet ? (  
-          <div>        
-            <Header 
-              nameOfPlayer={this.state.nameOfPlayer} 
-              totalPoints={this.state.totalPoints} 
-              pointsPerClick={this.state.pointsPerClick}
-              pointsPerSecond={this.state.pointsPerSecond}
-            />
-            
-            <Button 
-              onClick={() => this.increaseTotalPoints(this.state.pointsPerClick)} 
-              backgroundImage='url(http://reversespeech.s3.amazonaws.com/imgs/paris-hilton-celibate.jpg)'
-              className='clicker'
-            />
 
-            <CellPhone makeAnUpgrade={this.makeAnUpgrade}/>
-            <Extentions makeAnUpgrade={this.makeAnUpgrade}/>
-            <Nicole makeAnUpgrade={this.makeAnUpgrade}/> 
-            <Tinkerbell makeAnUpgrade={this.makeAnUpgrade}/>
-          </div>
-          ) : (
-          <div>
-            <Header />
-            <NameForm handleNameForm={this.handleNameForm}/>
-          </div>
-        )}
+          {this.state.playerIsSet ? (  
+            <div class="wrapper">       
+              <Header />
+
+              <PlayerStats
+                nameOfPlayer={nameOfPlayer} 
+                totalPoints={totalPoints} 
+                fame={fame}
+                pointsPerClick={pointsPerClick}
+                pointsPerSecond={pointsPerSecond}
+                automaticPointsMessage={automaticPointsMessage} />
+
+              <div className="clicker">
+                <Button className='clicker-img' onClick={this.increaseTotalPoints} 
+                  backgroundImage='url(https://orig00.deviantart.net/5499/f/2013/198/7/7/paris_hilton_png_by_brokenheartdesignz-d6du9ik.png)' />
+              </div>
+
+              <div className="upgrades">
+                <h2>Upgrades</h2>
+                <div className="upgrades-wrapper">
+                  <CellPhone makeAnUpgrade={this.makeAnUpgrade} totalPoints={totalPoints}/>
+                  <Extentions makeAnUpgrade={this.makeAnUpgrade} totalPoints={totalPoints}/>
+                  <Nicole makeAnUpgrade={this.makeAnUpgrade} totalPoints={totalPoints}/> 
+                  <Tinkerbell makeAnUpgrade={this.makeAnUpgrade} totalPoints={totalPoints}/>
+                </div>
+              </div>
+            </div>
+
+            ) : (
+
+            <div>
+              <Header />
+              <NameForm handleNameForm={this.handleNameForm} />
+              
+              <div>
+                <button className="test">hej</button>
+                <div className="visa-test">
+                  <p>testing</p>
+                </div>
+              </div>
+            </div>
+
+          )}
+          
       </div>
     );
   }
